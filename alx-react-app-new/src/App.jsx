a13a2234f6641d844840567d6b1ab7b37910d379
THERESE07 <niyigenatherese@gmail.com>
2025-03-10 10:50:50 +0200

    added changes

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -41,6 +41,9 @@ function App() {
       </div>
       <div>
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      </div>
+      <div>
+        <Counter />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
