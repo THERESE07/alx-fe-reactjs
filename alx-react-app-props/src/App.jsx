33ba3484bd4512e801f18538df06b636ab24177a
THERESE07 <niyigenatherese@gmail.com>
2025-03-10 12:09:02 +0200

    added changes

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'
-import ProfilePage from './ProfilePage'
-import UserContext from './UserContext'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
   const [count, setCount] = useState(0)
