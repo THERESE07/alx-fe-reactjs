8256bb8c4e614a4589abfc7afb1781caabe96f3e
THERESE07 <niyigenatherese@gmail.com>
2025-03-10 20:47:01 +0200

    added a folder

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,24 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 import useRecipeStore from './components/recipeStore'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeDetails from './components/RecipeDetails';
+import EditRecipeForm from './components/EditRecipeForm';
+import DeleteRecipeButton from './components/DeleteRecipeButton';
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <RecipeList />
-        <AddRecipeForm />
-        <useRecipeStore />
-      </div>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        {/* Other routes here */}
+      </Routes>
+      <RecipeList />
+      <AddRecipeForm />
+      <useRecipeStore />
+      <EditRecipeForm />
+      <DeleteRecipeButton />
+    </Router>
+  );
 }
+
+
 
 export default App
---
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -0,0 +1,32 @@
+import { useRecipeStore } from './recipeStore';
+import { useState } from 'react';
+
+const EditRecipeForm = ({ recipeId }) => {
+  const { recipes, updateRecipe } = useRecipeStore((state) => state);
+  const recipe = recipes.find((recipe) => recipe.id === recipeId);
+
+  const [title, setTitle] = useState(recipe.title);
+  const [description, setDescription] = useState(recipe.description);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateRecipe({ id: recipeId, title, description });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button type="submit">Save</button>
+    </form>
+  );
+};
+
+export default EditRecipeForm;
