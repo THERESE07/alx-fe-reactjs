9bae75f33e5cc8978764ce31939c05a676275f89
THERESE07 <niyigenatherese@gmail.com>
2025-03-10 08:14:59 +0200

    added changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +31,11 @@ function App() {
       </div>
       <div>
         <WelcomeMessage />
+      </div>
+      <div>
+        <Header />
+        <MainContent />
+        <Footer />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
