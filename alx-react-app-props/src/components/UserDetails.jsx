2e5f72b32febb13862719a0e7da3b5759d11e221
THERESE07 <niyigenatherese@gmail.com>
2025-03-10 12:02:48 +0200

    added changes

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import UserContext from '../UserContext';
+import React, { useContext } from 'react'
+import UserContext from '../UserContext'
 
 function UserDetails({ userData }) {
     const userData = useContext(UserContext); 
