dc1d5c7cb80254c8a5663539096652d651190e51
THERESE07 <niyigenatherese@gmail.com>
2025-03-22 16:53:14 +0200

    added changes

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,36 +1,78 @@
-import React from 'react';
+import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-
-const validationSchema = Yup.object({
-  username: Yup.string().required('Username is required'),
-  email: Yup.string().email('Invalid email').required('Email is required'),
-  password: Yup.string().required('Password is required'),
-});
-
-function FormikForm() {
-  return (
-    <Formik
-      initialValues={{ username: '', email: '', password: '' }}
-      validationSchema={validationSchema}
-      onSubmit={(values) => {
+function formikForm() {
+    // Validation schema
+    const validationSchema = Yup.object({
+        username: Yup.string().required('Username is required'),
+        email: Yup.string().email('Invalid email address').required('Email is required'),
+        password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+      });
+    
+      // Initial values for the form fields
+      const initialValues = {
+        username: '',
+        email: '',
+        password: '',
+      };
+    
+      // Handle form submission
+      const handleSubmit = (values) => {
         console.log('Form submitted:', values);
-      }}
-    >
-      {({ handleSubmit }) => (
-        <Form onSubmit={handleSubmit}>
-          <Field name="username" placeholder="Username" />
-          <ErrorMessage name="username" component="p" />
-          <Field name="email" placeholder="Email" />
-          <ErrorMessage name="email" component="p" />
-          <Field name="password" type="password" placeholder="Password" />
-          <ErrorMessage name="password" component="p" />
-          <button type="submit">Register</button>
-        </Form>
-        
-      )}
-    </Formik>
-  );
+      };
+    
+      return React.createElement(
+        'div',
+        null,
+        React.createElement('h2', null, 'Formik Registration Form'),
+        React.createElement(
+          Formik,
+          { initialValues, validationSchema, onSubmit: handleSubmit },
+          () =>
+            React.createElement(
+              Form,
+              null,
+              React.createElement(
+                'div',
+                null,
+                React.createElement('label', null, 'Username:'),
+                React.createElement(Field, { type: 'text', name: 'username' }),
+                React.createElement(ErrorMessage, {
+                  name: 'username',
+                  component: 'div',
+                  style: { color: 'red' },
+                })
+              ),
+              React.createElement(
+                'div',
+                null,
+                React.createElement('label', null, 'Email:'),
+                React.createElement(Field, { type: 'email', name: 'email' }),
+                React.createElement(ErrorMessage, {
+                  name: 'email',
+                  component: 'div',
+                  style: { color: 'red' },
+                })
+              ),
+              React.createElement(
+                'div',
+                null,
+                React.createElement('label', null, 'Password:'),
+                React.createElement(Field, { type: 'password', name: 'password' }),
+                React.createElement(ErrorMessage, {
+                  name: 'password',
+                  component: 'div',
+                  style: { color: 'red' },
+                })
+              ),
+              React.createElement(
+                'button',
+                { type: 'submit' },
+                'Register'
+              )
+            )
+        )
+      );
 }
 
-export default FormikForm;
+export default formikForm
