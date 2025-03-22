77c2dba523f6ae3639cea63467439e0e5737b073
THERESE07 <niyigenatherese@gmail.com>
2025-03-22 12:36:56 +0200

    added changes

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: {
     extend: {
-      
+      backgroundColor: ['hover', 'focus', 'active'],
     },
   },
   plugins: [],
