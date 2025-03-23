e36f9bb09a1da86bfb24c2636e5a212a3f522d7d
THERESE07 <niyigenatherese@gmail.com>
2025-03-23 10:58:31 +0200

    added changes

---
+++ recipe-sharing-platform/src/components/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div>
+        <h1 className="text-blue-500">Hello Dear Bestie</h1>
+      
+    </div>
+  )
+}
+
+export default Test
