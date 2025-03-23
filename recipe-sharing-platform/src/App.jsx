4b2207cf79ca5e40f2a759e66163140c12102357
THERESE07 <niyigenatherese@gmail.com>
2025-03-23 23:23:43 +0200

    added changes

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import HomePage from './components/HomePage'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeDetail from './components/RecipeDetail';
+import AddRecipeForm from './components/AddRecipeForm'
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/recipe/:id" element={<AddRecipeForm />} />
       </Routes>
     </Router>
   );
