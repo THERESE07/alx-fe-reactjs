19a3fc5335b1fe689f976adfbf5e668fd41ad2df
THERESE07 <niyigenatherese@gmail.com>
2025-04-11 15:19:00 +0200

    added changes

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import SearchDetails from "./components/SearchDetails";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <SearchDetails />
         <h1>GitHub User Search App</h1>
       </div>
     </>
---
+++ github-user-search/src/components/SearchDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchDetails() {
+  return (
+    <div>
+      SearchDetails
+    </div>
+  )
+}
+
+export default SearchDetails
